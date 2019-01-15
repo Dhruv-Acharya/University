a1447065f5278a5d12cc5b4af912dd5f72f8ebe9
makarajuyashwanthvarma <myvarma97@gmail.com>
2019-01-15 22:03:51 +0530

    Subject Entity Created

--- src/main/java/com/onboarding/University/entity/Subject.java
+++ src/main/java/com/onboarding/University/entity/Subject.java
@@ -1,6 +1,16 @@
 package com.onboarding.University.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name=Subject.TABLE_NAME)
 public class Subject {
+
+    public static final String TABLE_NAME="Subject";
+
+    @Id
     private String subjectId;
     private String  subjectName;
     private Department departmentId;
