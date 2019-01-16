c30d4c0c6b16d66f056187ca14b4510e53c9d8e3
yashParekh0909 <yash.parekh@coviam.com>
2019-01-16 10:29:00 +0530

    Custom queries written

--- src/main/java/com/onboarding/University/repository/ProfessorRepository.java
+++ src/main/java/com/onboarding/University/repository/ProfessorRepository.java
@@ -1,8 +1,16 @@
 package com.onboarding.University.repository;
 
 import com.onboarding.University.entity.Professor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProfessorRepository extends CrudRepository<Professor,String> {
 
+
+    @Query(name = "professorRanking", value = "select AVG(b.marks) from subject a inner join marks b on a.subject_id = b.subject_id group by a.professor_id")
+    List<Double> getProfessorRanking();
 }
+
